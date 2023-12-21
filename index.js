2a013449750bc80d7d9c8481516616d11881dc2b
Emerson Thiago <emersonthiago168@outlook.com>
2023-12-21 18:18:11 -0300

    criação da conta

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ function operation() {
         ])
         .then(answer => {
             const action = answer['action'];
-            console.log(action);
+
+            if (action === 'Criar Conta') createAccount();
         })
         .catch(err => console.log(err));
+}
+
+// create an account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir'));
 }
