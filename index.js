53862f2967753fe18ad2443090617283ca16db80
Emerson Thiago <emersonthiago168@outlook.com>
2023-12-21 18:13:09 -0300

    inicialização do sistema

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ const chalk = require('chalk');
 // módulos internos
 const fs = require('fs');
 
-console.log('Iniciamos o Accounts')
+operation();
+
+function operation() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'O que você deseja fazer?',
+                choices: ['Criar Conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
+            }
+        ])
+        .then(answer => {
+            const action = answer['action'];
+            console.log(action);
+        })
+        .catch(err => console.log(err));
+}
